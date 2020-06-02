585c27e2d3f2704cd17753ea32855a842f6b7520
Luna <1017929760@qq.com>
2020-06-02 18:08:44 +0800

    Money.tsx里的CategorySection UI完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -48,7 +48,27 @@ const NotesSection = styled.section`
 `;
 
 const CategorySection = styled.section`
-
+  font-size: 24px;
+  >ul{
+    display:flex;
+    background:rgb(138,165,205);
+    >li{
+      width: 50%;
+      text-align:center;
+      padding: 16px 0;
+      position: relative;
+      &.selected::after{
+        content:'';
+        display:block;
+        height: 3px;
+        background:rgb(45,95,170);
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        left: 0;
+      }
+    }
+  }
 `;
 
 const NumberPadSection = styled.section`
@@ -75,7 +95,7 @@ function Money() {
       </NotesSection>
       <CategorySection>
         <ul>
-          <li>支出</li>
+          <li className='selected'>支出</li>
           <li>收入</li>
         </ul>
       </CategorySection>

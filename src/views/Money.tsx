0fc502fad4e1356bca9854aa485eac289821e65b
Luna <1017929760@qq.com>
2020-06-02 17:56:37 +0800

    Money.tsx里的NotesSection UI完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -27,7 +27,24 @@ const TagsSection = styled.section`
 `;
 
 const NotesSection = styled.section`
-
+  background:rgb(240,240,240);
+  padding: 0 16px;
+  font-size: 14px;
+  >label{
+    display:flex;
+    align-items:center;
+    >span{
+      margin-right: 16px;
+      white-space: nowrap;
+    }
+    >input{
+      display:block;
+      width: 100%;
+      height: 72px;
+      background:none;
+      border: none;
+    }
+  }
 `;
 
 const CategorySection = styled.section`
@@ -53,7 +70,7 @@ function Money() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type="text"/>
+          <input type="text" placeholder='在这里添加备注'/>
         </label>
       </NotesSection>
       <CategorySection>

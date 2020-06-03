e1ca2702542949cffeafacf5fdd0ad475e1ba59e
Luna <1017929760@qq.com>
2020-06-03 18:44:07 +0800

    重构表单数据收集

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,56 +1,39 @@
 import Layout from '../components/Layout';
 import React, {useState} from 'react';
 import styled from 'styled-components';
-import { TagsSection } from './Money/TagsSection';
-import { NoteSection } from './Money/NoteSection';
-import { CategorySection } from './Money/CategorySection';
-import { NumberPadSection } from './Money/NumberPadSection';
+import {TagsSection} from './Money/TagsSection';
+import {NoteSection} from './Money/NoteSection';
+import {CategorySection} from './Money/CategorySection';
+import {NumberPadSection} from './Money/NumberPadSection';
 
 const MyLayout = styled(Layout)`
   display:flex;
   flex-direction: column;
 `;
 
-type Category = '-'|'+'
+type Category = '-' | '+'
+
 function Money() {
-  const [selected,setSelected] = useState({
+  const [selected, setSelected] = useState({
     tags: [] as string[],
     note: '',
     category: '-' as Category,
     amount: 0
-  })
+  });
+  const onChange = (obj: Partial<typeof selected>) => {
+    setSelected({...selected, ...obj});
+  };
   return (
     <MyLayout>
-      {selected.tags.join(',')}
-      <hr/>
-      {selected.note}
-      <hr/>
-      {selected.category}
-      <hr/>
-      {selected.amount}
-      <hr/>
       <TagsSection value={selected.tags}
-                   onChange={(tags)=>setSelected({
-                     ...selected,
-                     tags:tags
-                   })}/>
+                   onChange={tags => onChange({tags})}/>
       <NoteSection value={selected.note}
-                   onChange={(note)=>setSelected({
-                     ...selected,
-                     note:note
-                   })}/>
+                   onChange={note => onChange({note})}/>
       <CategorySection value={selected.category}
-                       onChange={(category)=>setSelected({
-                         ...selected,
-                         category:category
-                       })}/>
+                       onChange={category => onChange({category})}/>
       <NumberPadSection value={selected.amount}
-                        onChange={(amount)=>setSelected({
-                          ...selected,
-                          amount:amount
-                        })}
-                        onOk={()=>{}}
-      />
+                        onChange={amount => onChange({amount})}
+                        onOk={() => {}}/>
     </MyLayout>
   );
 }

889d1e920c6123e6adef68c92c8a3de20a6b6072
Luna <1017929760@qq.com>
2020-06-04 16:48:57 +0800

    创建/tags/:tag路由，学会exact的用法

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Money from 'views/Money';
 import Statistics from 'views/Statistics';
 import NoMatch from 'views/NoMatch';
 import styled from 'styled-components';
+import { TagEdit } from 'views/TagEdit';
 
 const AppWrapper = styled.div`
  color: #333;
@@ -20,13 +21,16 @@ function App() {
     <AppWrapper>
       <Router>
         <Switch>
-          <Route path="/tags">
+          <Route exact path="/tags">
             <Tags/>
           </Route>
-          <Route path="/money">
+          <Route exact path="/tags/:tag">
+            <TagEdit/>
+          </Route>
+          <Route exact path="/money">
             <Money/>
           </Route>
-          <Route path="/statistics">
+          <Route exact path="/statistics">
             <Statistics/>
           </Route>
           <Redirect exact from='/' to='money'/>

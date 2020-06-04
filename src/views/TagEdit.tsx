4fcd023d468ee6e3e20ce1b99d54ce32f9860f18
Luna <1017929760@qq.com>
2020-06-04 22:59:00 +0800

    编辑标签名

--- src/views/TagEdit.tsx
+++ src/views/TagEdit.tsx
@@ -26,9 +26,9 @@ const InputWrapper = styled.div`
   margin-top: 8px;
 `;
 const TagEdit: React.FC = () => {
-  const {findTag} = useTags();
-  let {id} = useParams<Params>();
-  const tag = findTag(parseInt(id));
+  const {findTag,updateTag} = useTags();
+  let {id:idString} = useParams<Params>();
+  const tag = findTag(parseInt(idString));
   return (
     <Layout>
       <Topbar>
@@ -37,7 +37,11 @@ const TagEdit: React.FC = () => {
         <Icon/>
       </Topbar>
       <InputWrapper>
-        <Input label='标签名' type='text' value={tag.name}/>
+        <Input label='标签名' type='text' value={tag.name}
+               onChange={(e)=>{
+                 updateTag(tag.id,{name:e.target.value});
+               }}
+        />
       </InputWrapper>
       <Center>
         <Space/>

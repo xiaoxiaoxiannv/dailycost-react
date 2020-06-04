8d0d480a78b105665663c6ef7f7221e74384a09b
Luna <1017929760@qq.com>
2020-06-04 22:10:03 +0800

    创建单独的Button组件

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background:#5673a2;
+  border-radius: 4px;
+  color: white;
+`;
+export {Button}
--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,51 +1,27 @@
 import styled from 'styled-components';
-import React, {useRef} from 'react';
+import React, {ChangeEventHandler} from 'react';
+import {Input} from '../../components/Input';
 
 const Wrapper = styled.section`
   background:rgb(240,240,240);
   padding: 0 16px;
   font-size: 14px;
-  >label{
-    display:flex;
-    align-items:center;
-    >span{
-      margin-right: 16px;
-      white-space: nowrap;
-    }
-    >input{
-      display:block;
-      width: 100%;
-      height: 72px;
-      background:none;
-      border: none;
-    }
-  }
 `;
 
-type Props ={
+type Props = {
   value: string,
-  onChange: (value: string) =>void;
+  onChange: (value: string) => void;
 }
-const NoteSection:React.FC<Props> = (props)=>{
+const NoteSection: React.FC<Props> = (props) => {
   const note = props.value;
-  const refInput = useRef<HTMLInputElement>(null);
-  const onBlur = ()=>{
-    if(refInput.current!==null){
-      props.onChange(refInput.current.value)
-    }
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    props.onChange(e.target.value);
   };
-  return(
+return (
     <Wrapper>
-      <label>
-        <span>备注</span>
-        <input type="text" placeholder='在这里添加备注'
-               ref={refInput}
-               defaultValue={note}
-               onBlur={onBlur}
-        />
-      </label>
-    </Wrapper>
-  )
+        <Input label='备注' type='text' placeholder='请添加备注' value={note} onChange={onChange}/>
+    </Wrapper >
+);
 };
 
 export {NoteSection};

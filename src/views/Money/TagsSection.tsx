0006eaa0deeca2c4ea6aaf0291ffab87abf8fa0a
Luna <1017929760@qq.com>
2020-06-04 17:30:31 +0800

    重构:将tag:string重构为tag:{id:number;name:string}

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -35,39 +35,39 @@ const Wrapper = styled.section`
 `;
 
 type Props = {
-  value: string[],
-  onChange: (value: string[]) => void;
+  value: number[],
+  onChange: (value: number[]) => void;
 }
 
-const TagsSection: React.FC<Props> = (props)=>{
-  const{tags,setTags} = useTags();
-  const selectedTags = props.value;
-  const onAddTag = ()=>{
-    const tagName =window.prompt('新标签的名称为');
-    if(tagName!== null){
-      setTags([...tags,tagName])
+const TagsSection: React.FC<Props> = (props) => {
+  const {tags, setTags} = useTags();
+  const selectedTagIds = props.value;
+  const onAddTag = () => {
+    const tagName = window.prompt('新标签的名称为');
+    if (tagName !== null) {
+      setTags([...tags, {id: Math.random(), name: tagName}]);
     }
-  }
-  const onToggleTag = (tag:string)=>{
-    const index = selectedTags.indexOf(tag);
-    if(index >=0){
-      props.onChange(selectedTags.filter(t=>t!==tag));
-    }else{
-      props.onChange([...selectedTags,tag])
+  };
+  const onToggleTag = (tagId: number) => {
+    const index = selectedTagIds.indexOf(tagId);
+    if (index >= 0) {
+      props.onChange(selectedTagIds.filter(t => t !== tagId));
+    } else {
+      props.onChange([...selectedTagIds, tagId]);
     }
-  }
-  const getClass = (tag:string)=>selectedTags.indexOf(tag)>=0?'selected':'';
-  return(
+  };
+  const getClass = (tagId: number) => selectedTagIds.indexOf(tagId) >= 0 ? 'selected' : '';
+  return (
     <Wrapper>
       <ol>
         {tags.map(tag =>
-        <li key={tag} onClick={
-          ()=>{onToggleTag(tag)}
-        } className={getClass(tag)}>{tag}</li>
+          <li key={tag.id} onClick={
+            () => {onToggleTag(tag.id);}
+          } className={getClass(tag.id)}>{tag.name}</li>
         )}
       </ol>
       <button onClick={onAddTag}>新增标签</button>
     </Wrapper>
-  )
-}
+  );
+};
 export {TagsSection};

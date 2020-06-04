94820d9b3204ff3277548e931692d14b6ab5e710
Luna <1017929760@qq.com>
2020-06-04 18:24:59 +0800

    封装findTag 到useTags里

--- src/views/TagEdit.tsx
+++ src/views/TagEdit.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import {useTags} from '../useTags';
+import {useParams} from 'react-router-dom';
 
-const TagEdit:React.FC = ()=>{
-  return(
-    <div>hi</div>
-  )
+type Params = {
+  id: string
+}
+const TagEdit: React.FC = () => {
+  const {findTag} = useTags();
+  let {id} = useParams<Params>();
+  const tag = findTag(parseInt(id));
+  return (
+    <div>{tag.name}</div>
+  );
 };
 
-export {TagEdit}
+export {TagEdit};

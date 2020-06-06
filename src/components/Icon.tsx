663556fe91345ddade74f9abbd3e82d28f2d8498
Luna <1017929760@qq.com>
2020-06-06 20:22:17 +0800

    点击回退

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import cs from 'classnames';
 
 let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
 
-type Props ={
-  name?:string
-}
-const Icon = (props:Props)=>{
-  return(
-    <svg className='icon'>
-      {props.name && <use xlinkHref={'#'+props.name}/>}
+type Props = {
+  name?: string
+} & React.SVGAttributes<SVGElement>
+const Icon = (props: Props) => {
+  const {name, children, className, ...rest} = props;
+  return (
+    <svg className={cs('icon', className)} {...rest}>
+      {props.name && <use xlinkHref={'#' + props.name}/>}
     </svg>
-  )
+  );
 };
 export default Icon;

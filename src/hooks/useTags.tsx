b903aea06ffdd5dd8890cbbc882a7638bb89b382
Luna <1017929760@qq.com>
2020-06-06 22:04:23 +0800

    add record

--- src/hooks/useTags.tsx
+++ src/hooks/useTags.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from 'react';
 import {createId} from 'lib/createId';
-import {useUpdate} from './hooks/useUpdate';
+import {useUpdate} from './useUpdate';
 
 const useTags = () => {
   const [tags, setTags] = useState<{ id: number; name: string }[]>([]);
@@ -16,6 +16,7 @@ const useTags = () => {
     }
     setTags(localTags);
   }, []);
+
   useUpdate(() => {
     window.localStorage.setItem('tags', JSON.stringify(tags));
   }, [tags]);
--- src/views/TagEdit.tsx
+++ src/views/TagEdit.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {useTags} from 'useTags';
+import {useTags} from 'hooks/useTags';
 import {useParams, useHistory} from 'react-router-dom';
 import Layout from 'components/Layout';
 import Icon from 'components/Icon';
 import {Button} from 'components/Button';
 import styled from 'styled-components';
-import {Input} from '../components/Input';
-import {Center} from '../components/Center';
-import {Space} from '../components/Space';
+import {Input} from 'components/Input';
+import {Center} from 'components/Center';
+import {Space} from 'components/Space';
 
 type Params = {
   id: string

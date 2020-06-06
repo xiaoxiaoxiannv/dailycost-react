6c8887d946ced1237e95d5c02f3ecdcff36e056a
Luna <1017929760@qq.com>
2020-06-06 21:16:00 +0800

    tags 数据持久化

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -41,14 +41,9 @@ type Props = {
 }
 
 const TagsSection: React.FC<Props> = (props) => {
-  const {tags, setTags} = useTags();
+  const {tags, addTag} = useTags();
   const selectedTagIds = props.value;
-  const onAddTag = () => {
-    const tagName = window.prompt('新标签的名称为');
-    if (tagName !== null) {
-      setTags([...tags, {id: createId(), name: tagName}]);
-    }
-  };
+
   const onToggleTag = (tagId: number) => {
     const index = selectedTagIds.indexOf(tagId);
     if (index >= 0) {
@@ -67,7 +62,7 @@ const TagsSection: React.FC<Props> = (props) => {
           } className={getClass(tag.id)}>{tag.name}</li>
         )}
       </ol>
-      <button onClick={onAddTag}>新增标签</button>
+      <button onClick={addTag}>新增标签</button>
     </Wrapper>
   );
 };
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -25,14 +25,14 @@ const TagList = styled.ol`
 `;
 
 function Tags() {
-  const{tags} = useTags();
+  const{tags,addTag} = useTags();
   return (
     <Layout>
       <TagList>
         {tags.map(tag =>
         <li key={tag.id}>
           <Link to={'/tags/'+tag.id}>
-            <span className='oneLine'>{tag.name}</span>
+            <span className='oneLine'>{tag.id}:{tag.name}</span>
             <Icon name='right'/>
           </Link>
         </li>
@@ -42,7 +42,7 @@ function Tags() {
         <Space/>
         <Space/>
         <Space/>
-        <Button>新建标签</Button>
+        <Button onClick={addTag}>新建标签</Button>
       </Center>
     </Layout>
   );

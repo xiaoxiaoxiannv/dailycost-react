3891779d5ee01bcc127fcdd3242fb07d284b8530
Luna <1017929760@qq.com>
2020-06-08 14:38:32 +0800

    展示所有记录

--- src/hooks/useTags.tsx
+++ src/hooks/useTags.tsx
@@ -43,6 +43,10 @@ const useTags = () => {
       setTags([...tags, {id: createId(), name: tagName}]);
     }
   };
-  return {tags, addTag, setTags, findTag, findTagIndex, updateTag, deleteTag};
+  const getName = (id: number) => {
+    const tag = tags.filter(t => t.id === id)[0];
+    return tag ? tag.name : '';
+  };
+  return {tags, getName,addTag, setTags, findTag, findTagIndex, updateTag, deleteTag};
 };
 export {useTags};

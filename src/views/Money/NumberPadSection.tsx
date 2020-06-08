da8e2f05dd808ff8b897b14b099fc389e872e166
Luna <1017929760@qq.com>
2020-06-08 11:59:51 +0800

    解决无法输入点的bug

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,37 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Wrapper} from './NumberPadSection/Wrapper';
 import {generateOutput} from './NumberPadSection/gengrateOutput';
 
 type Props = {
   value: number,
   onChange: (value: number) => void;
-  onOk?: ()=>void;
+  onOk?: () => void;
 }
-const NumberPadSection:React.FC<Props> = (props)=>{
-  const output = props.value.toString();
-  const setOutput = (output:string)=>{
-    let value;
-    if(output.length>16){
-      value = parseFloat(output.slice(0,16));
-    }else if(output.length === 0){
-      value = 0;
-    }else{
-      value = parseFloat(output)
+const NumberPadSection: React.FC<Props> = (props) => {
+  //const output = props.value.toString();
+  const [output, _setOutput] = useState(props.value.toString());
+  const setOutput = (output: string) => {
+    let newOutput: string;
+    if (output.length > 16) {
+      newOutput = output.slice(0, 16);
+    } else if (output.length === 0) {
+      newOutput = '0';
+    } else {
+      newOutput = output;
     }
-    props.onChange(value);
+    _setOutput(newOutput);
+    props.onChange(parseFloat(newOutput));
   };
-  const onClickButtonWrapper = (e:React.MouseEvent) =>{
+  const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
-    if(text === null){return;}
-    if(text === 'OK'){
-      if(props.onOk){props.onOk()}
+    if (text === null) {return;}
+    if (text === 'OK') {
+      if (props.onOk) {props.onOk();}
       return;
     }
-    if('0123456789.'.split('').concat(['删除','清空']).indexOf(text)>=0){
-      setOutput(generateOutput(text,output));
+    if ('0123456789.'.split('').concat(['删除', '清空']).indexOf(text) >= 0) {
+      setOutput(generateOutput(text, output));
     }
   };
-  return(
+  return (
     <Wrapper>
       <div className='output'>{output}</div>
       <div className='pad clearfix' onClick={onClickButtonWrapper}>
@@ -51,6 +53,6 @@ const NumberPadSection:React.FC<Props> = (props)=>{
         <button className='dot'>.</button>
       </div>
     </Wrapper>
-  )
+  );
 };
 export {NumberPadSection};
